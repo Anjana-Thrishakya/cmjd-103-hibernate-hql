6ca51e86f71ff251129ea5b9441785659fbd2ca5
Anjana Thrishakya <anjanathrishakya@gmail.com>
2023-08-27 10:39:57 +0530

    ORDER BY, Aggrigate Functions, GROUP BY

--- src/App.java
+++ src/App.java
@@ -13,25 +13,38 @@ public class App {
 
         // List<CustomerEntity> customerEntities = repository.getAllCustomers();
         // customerEntities.forEach(e -> {
-        //     System.out.println(e);
+        // System.out.println(e);
         // });
 
         // List<Object[]> data = repository.getAllCustomersByNative();
         // data.forEach(e-> {
-        //     System.out.println(Arrays.toString(e));
+        // System.out.println(Arrays.toString(e));
         // });
 
         // List<Object[]> data = repository.getCustomerNameAndDob();
         // data.forEach(e-> {
-        //     System.out.println(Arrays.toString(e));
+        // System.out.println(Arrays.toString(e));
         // });
 
         // CustomerEntity c1 = repository.getCustomerByHQL("C005");
         // System.out.println(c1.toString());
 
-        CustomerEntity c1 = repository.getCustomerByHQLUsingNamedParameterd("C005");
-        System.out.println(c1.toString());
+        // CustomerEntity c1 = repository.getCustomerByHQLUsingNamedParameterd("C005");
+        // System.out.println(c1.toString());
 
-        
+        // List<CustomerEntity> customerEntities =
+        // repository.getAllCustomersOrderByNameDesc();
+        // customerEntities.forEach(e -> {
+        // System.out.println(e);
+        // });
+
+        // Object[] data = repository.getCustomerSummery();
+        // System.out.println(Arrays.toString(data));
+
+        List<Object[]> data = repository.getCustomerSummeryByProvince();
+        data.forEach(e -> {
+            System.out.println(Arrays.toString(e));
+        });
+
     }
 }
--- src/repository/CustomerRepository.java
+++ src/repository/CustomerRepository.java
@@ -11,12 +11,12 @@ import util.SessionFactoryConfiguration;
 public class CustomerRepository {
     Session session = SessionFactoryConfiguration.getInstance().getSession();
 
-    public CustomerEntity getCustomer(String id){
+    public CustomerEntity getCustomer(String id) {
         CustomerEntity customerEntity = session.get(CustomerEntity.class, id);
         return customerEntity;
     }
 
-    public List<CustomerEntity> getAllCustomers(){
+    public List<CustomerEntity> getAllCustomers() {
         String hql = "FROM CustomerEntity";
         Query query = session.createQuery(hql);
         List<CustomerEntity> customerEntities = query.list();
@@ -24,7 +24,7 @@ public class CustomerRepository {
 
     }
 
-    public List<Object[]> getAllCustomersByNative(){
+    public List<Object[]> getAllCustomersByNative() {
         String sql = "SELECT * FROM Customer";
         Query query = session.createSQLQuery(sql);
         List<Object[]> data = query.list();
@@ -32,7 +32,7 @@ public class CustomerRepository {
 
     }
 
-    public List<Object[]> getCustomerNameAndDob(){
+    public List<Object[]> getCustomerNameAndDob() {
         String hql = "SELECT C.id, C.name, C.dob FROM CustomerEntity C";
         Query query = session.createQuery(hql);
         List<Object[]> data = query.list();
@@ -40,18 +40,43 @@ public class CustomerRepository {
 
     }
 
-    public CustomerEntity getCustomerByHQL(String custId){
+    public CustomerEntity getCustomerByHQL(String custId) {
         String hql = "FROM CustomerEntity WHERE id= '" + custId + "'";
         Query query = session.createQuery(hql);
         CustomerEntity customerEntity = (CustomerEntity) query.uniqueResult();
         return customerEntity;
     }
 
-    public CustomerEntity getCustomerByHQLUsingNamedParameterd(String custId){
+    public CustomerEntity getCustomerByHQLUsingNamedParameterd(String custId) {
         String hql = "FROM CustomerEntity WHERE id=:id";
         Query query = session.createQuery(hql);
         query.setParameter("id", custId);
         CustomerEntity customerEntity = (CustomerEntity) query.uniqueResult();
         return customerEntity;
+    }
+
+    public List<CustomerEntity> getAllCustomersOrderByNameDesc(){
+        String hql = "FROM CustomerEntity ORDER BY name DESC";
+        Query query = session.createQuery(hql);
+        List<CustomerEntity> customerEntities = query.list();
+        return customerEntities;
+    }
+
+    //SELECT Count(CustID), SUM(salary), AVG(salary),  MAX(salary), MIN(salary) FROM Customer;
+
+    public Object[] getCustomerSummery() {
+        String hql = "SELECT count(id), sum(salary), avg(salary), max(salary), min(salary) FROM CustomerEntity";
+        Query query = session.createQuery(hql);
+        Object[] data = (Object[]) query.uniqueResult();
+        return data;
+    }
+
+    //SELECT province, Count(CustID), SUM(salary), AVG(salary),  MAX(salary), MIN(salary) FROM Customer GROUP BY province;
+
+    public List<Object[]> getCustomerSummeryByProvince() {
+        String hql = "SELECT province ,count(id), sum(salary), avg(salary), max(salary), min(salary) FROM CustomerEntity GROUP BY province";
+        Query query = session.createQuery(hql);
+        List<Object[]> data =  query.list();
+        return data;
     }
 }
